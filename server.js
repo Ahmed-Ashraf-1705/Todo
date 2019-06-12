15d26bb92b6576dcb8e71032f295ae8ca0da71d8
Ahmed-Ashraf-1705 <51462752+Ahmed-Ashraf-1705@users.noreply.github.com>
2019-06-12 15:40:18 +0200

    heroku deployment

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ app.use('/users',user);
 app.get('/',(req,res)=>{
   res.send('Invalid Endpoint!');
 });
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname+'public/index.html'));
+});
 //  app.get('*',req,res)=>{
 //    res.sendFile(path.joi(__dirname,'public/index.html'));
 //  }
